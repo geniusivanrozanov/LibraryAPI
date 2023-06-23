9a0e7707129d43001dd61d49187e5289780e63bd
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-24 00:57:18 +0300

    add database auto migration

--- LibraryAPI.DAL/Extensions/ServiceExtensions.cs
+++ LibraryAPI.DAL/Extensions/ServiceExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace LibraryAPI.DAL.Extensions;
 
@@ -14,16 +15,40 @@ public static class ServiceExtensions
         return services;
     }
     
-    public static IServiceCollection ConfigureLibraryDbContext(this IServiceCollection services, IConfiguration configuration)
+    private static IServiceCollection ConfigureLibraryDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         var libraryConnectionString = configuration.GetConnectionString("LibraryConnectionString");
         ArgumentNullException.ThrowIfNull(libraryConnectionString);
-
+        
         services.AddDbContext<LibraryDbContext>(options =>
         {
             options.UseNpgsql(libraryConnectionString);
         });
+
+        services.BuildServiceProvider()
+            .MigrateDatabase<LibraryDbContext>();
         
         return services;
+    }
+
+    private static IServiceProvider MigrateDatabase<TContext>(this IServiceProvider serviceProvider)
+        where TContext : DbContext
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        try
+        {
+            context.Database.Migrate();
+        }
+        catch (Exception)
+        {
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<TContext>>();
+            logger.LogError("An error occurred while migrating the database");
+            
+            throw;
+        }
+
+        return serviceProvider;
     }
 }
