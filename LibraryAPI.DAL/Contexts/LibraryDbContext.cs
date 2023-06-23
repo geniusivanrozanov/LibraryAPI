480d5fe27fb811d194ed53e294148b2b6d5f1b39
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-23 23:41:08 +0300

    add library database context

---
+++ LibraryAPI.DAL/Contexts/LibraryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using LibraryAPI.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.DAL.Contexts;
+
+public class LibraryDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+{
+    public DbSet<Book> Books { get; set; } = default!;
+
+    public DbSet<Genre> Genres { get; set; } = default!;
+
+    public DbSet<Author> Authors { get; set; } = default!;
+
+    public DbSet<BookRent> BookRents { get; set; } = default!;
+
+    public LibraryDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
