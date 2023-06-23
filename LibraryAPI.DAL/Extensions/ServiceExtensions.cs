5a385703e98bbc23de98b9ccdc2329a595bcc19c
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-23 23:55:31 +0300

    add library database context configuration

---
+++ LibraryAPI.DAL/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using LibraryAPI.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryAPI.DAL.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureLibraryDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var libraryConnectionString = configuration.GetConnectionString("LibraryConnectionString");
+        ArgumentNullException.ThrowIfNull(libraryConnectionString);
+
+        services.AddDbContext<LibraryDbContext>(options =>
+        {
+            options.UseNpgsql(libraryConnectionString);
+        });
+        
+        return services;
+    }
+}
