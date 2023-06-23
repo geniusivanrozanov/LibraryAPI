385dc5cf2b3a1da4f0909f4e4c2a3378357c408f
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-24 01:37:57 +0300

    add base repository interface

---
+++ LibraryAPI.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using LibraryAPI.DAL.Interfaces.Entities;
+
+namespace LibraryAPI.DAL.Interfaces;
+
+public interface IRepository<TEntity, TKey>
+    where TEntity : class, IBaseEntity<TKey>
+{
+    IQueryable<TEntity> Get();
+
+    IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> condition);
+
+    void Create(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+}
