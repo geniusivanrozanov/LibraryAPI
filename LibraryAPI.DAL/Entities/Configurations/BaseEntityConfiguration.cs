3c993a69d4e32f3bcabddb00a6b41502090a347b
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-23 22:54:54 +0300

    add base entity configuration template

---
+++ LibraryAPI.DAL/Entities/Configurations/BaseEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using LibraryAPI.DAL.Interfaces.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraryAPI.DAL.Entities.Configurations;
+
+public abstract class BaseEntityConfiguration<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
+    where TEntity : class, IBaseEntity<TKey>
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+    }
+}
