f68bd9c370af51daf4e15a6fa5f1b52996fb2f02
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-24 01:58:07 +0300

    add base repository implementation

---
+++ LibraryAPI.DAL/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using LibraryAPI.DAL.Interfaces;
+using LibraryAPI.DAL.Interfaces.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.DAL.Repositories;
+
+public abstract class Repository<TEntity, TKey, TContext> : IRepository<TEntity, TKey>
+    where TEntity : class, IBaseEntity<TKey>
+    where TContext : DbContext
+{
+    protected readonly TContext Context;
+
+    protected Repository(TContext context)
+    {
+        Context = context;
+    }
+
+    public IQueryable<TEntity> Get() => Context.Set<TEntity>();
+
+    public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> condition) => Context.Set<TEntity>().Where(condition);
+
+    public void Create(TEntity entity) => Context.Set<TEntity>().Add(entity);
+
+    public void Update(TEntity entity) => Context.Set<TEntity>().Update(entity);
+
+    public void Delete(TEntity entity) => Context.Set<TEntity>().Remove(entity);
+}
