1901d02807ea60188974a0cc074bf382a1fec601
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-26 23:49:54 +0300

    add repository manager

---
+++ LibraryAPI.DAL/Interfaces/Repositories/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryAPI.DAL.Interfaces.Repositories;
+
+public interface IRepositoryManager
+{
+    IAuthorRepository Authors { get; }
+    IBookRentRepository BookRents { get; }
+    IBookRepository Books { get; }
+    IGenreRepository Genres { get; }
+
+    void Save();
+    Task SaveAsync();
+}
