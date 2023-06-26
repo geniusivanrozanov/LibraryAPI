0f73dd292e462c4872c029c9a4df3c0c5b564a2a
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-27 02:24:48 +0300

    add automapper to project

---
+++ LibraryAPI.BLL/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryAPI.BLL.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+    {
+        services.ConfigureAutomapper();
+        
+        return services;
+    }
+    
+    private static IServiceCollection ConfigureAutomapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        
+        return services;
+    }
+}
